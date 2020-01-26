d4045968bb286e11e8984a546ddfce82bfaa9834
Denis Danilkov <kvantokost@gmail.com>
2020-01-27 02:38:14 +0300

    feat: redux actions are defined

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,32 +1,71 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const initialState = {
-    todoLists: []
-  };
+  todoLists: []
+};
 
 function reducer(state = initialState, action) {
-    console.log('reducer', state, action);
-    switch(action.type) {
-        case 'CREATE_TODO-LIST':
-          return {
-            todoLists: [...state.todoLists, {title: action.title, id: (state.todoLists.length + 1), todos: []}]
-          };
-        case 'DELETE_TODO-LIST':
-          return {
-            todoLists: state.todoLists.filter(item => {item.id !== action.id})
-          };
-        case 'ADD_TODO':
-          return {
-            todoLists:  todoLists.map(item, i => {
-                if ((action.id - 1) === i) {return item.todos.concat({title: action.title, id: item.todos + 1, done: false})}
-                else {return item} 
-            })  
-          };
-        default:
-          return state;
-      }
+  console.log("reducer", state, action);
+  switch (action.type) {
+    case "CREATE_TODO-LIST":
+      return {
+        todoLists: [
+          ...state.todoLists,
+          { title: action.title, id: state.todoLists.length + 1, todos: [] }
+        ]
+      };
+    case "DELETE_TODO-LIST":
+      return {
+        todoLists: state.todoLists.filter(item => {
+          item.id !== action.id;
+        })
+      };
+    case "ADD_TODO":
+      return {
+        todoLists: state.todoLists.map((item, i) => {
+          if (action.id - 1 === i) {
+            return item.todos.concat({
+              title: action.title,
+              id: item.todos + 1,
+              done: false
+            });
+          } else {
+            return item;
+          }
+        })
+      };
+    case "DELETE_TODO":
+      return {
+        todoLists: state.todoLists.map((item, i) => {
+          if (action.id - 1 === i) {
+            return ({
+              title: item.title,
+              id: item.id,
+              todos: item.todos.filter((member, i) => i !== action.todo_id - 1)
+            });
+          } else {
+            return item;
+          }
+        })
+      };
+    case "DONE_TODO":
+      return {
+        todoLists: state.todoLists.map(item, i => {
+            if (action.id - 1 === i) {
+              return ({
+                title: item.title,
+                id: item.id,
+                todos: item.todos.map((member, i) => i === action.todo_id - 1 ? {title: member.title, id: member.id, done: !member.done} : member)
+              });
+            } else {
+              return item;
+            }
+          })
+      };
+    default:
+      return state;
   }
+}
 
-   const store = createStore(reducer);
-   export default store;
-
+const store = createStore(reducer);
+export default store;
--- src/components/todolists/todoList.js
+++ src/components/todolists/todoList.js
@@ -2,30 +2,48 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 
 function mapStateToProps(state) {
-    return {
-      todoLists: state.todoLists
-    };
-  }
+  return {
+    todoLists: state.todoLists
+  };
+}
 
 function TodoList(props) {
-    
- const [todo, setTodo] = useState('');
-  
+  const [todo, setTodo] = useState("");
+
   return (
-    <div className='TodoList'>
+    <div className="TodoList">
       <span>{props.title}</span>
-      <input onChange={e => {setTodo(e.target.value)}} placeholder='Add your Todo' type='text'></input><button onClick={() => {props.dispatch({ type: "ADD_TODO", id: props.id, title: todo })}}>Add Todo</button>
-      <button onClick={() => {props.dispatch({ type: "DELETE_TODO-LIST", id: props.id })}}>Delete</button>
+      <input
+        onChange={e => {
+          setTodo(e.target.value);
+        }}
+        placeholder="Add your Todo"
+        type="text"
+      ></input>
+      <button
+        onClick={() => {
+          props.dispatch({ type: "ADD_TODO", id: props.id, title: todo });
+        }}
+      >
+        Add Todo
+      </button>
+      <button
+        onClick={() => {
+          props.dispatch({ type: "DELETE_TODO-LIST", id: props.id });
+        }}
+      >
+        Delete
+      </button>
       <ul className="tasks">
-        {props.items.map(item => (
+        {props.todoLists[props.id - 1].todos.map(item => (
           <li key={item.id}>
             {item.title}
             <button
-              onClick={handleDelete.bind(item._id)}
+              onClick={() => {props.dispatch({ type: "DELETE_TODO", id: props.id, todo_id: item.id})}}
               className="delete"
             ></button>
             <button
-              onClick={handleStatus.bind(item.id)}
+              onClick={() => {props.dispatch({ type: "DONE_TODO", id: props.id, todo_id: item.id})}}
               className="checked"
             ></button>
           </li>
@@ -35,4 +53,4 @@ function TodoList(props) {
   );
 }
 
-export default connect(mapStateToProps)(TodoList);;
+export default connect(mapStateToProps)(TodoList);
